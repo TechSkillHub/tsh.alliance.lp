510afe933650b65c40a28040d8296bb301c0ba79
Felipe Carvalho <felipe.carvalho@fullbar.com.br>
2023-08-01 17:59:21 -0300

    Modalidades

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,10 @@
 <template>
-  <Cookies />
-  <Transition>
-    <ModalDefault v-if="modalInfo.show" :modalInfo="modalInfo" />
-  </Transition>
-  <transition name="fade" mode="out-in">
-    <Loading v-if="loading" />
-  </transition>
   <Header />
   <RouterView />
   <Footer />
 </template>
 
 <script>
-import Cookies from '@/common/cookies/Cookies.vue'
-import ModalDefault from '@/common/modal/Default.vue'
-import Loading from '@/common/loading/Loading.vue'
 import Header from '@/common/header/Header.vue'
 import Footer from '@/common/footer/Footer.vue'
 import { RouterView } from 'vue-router'
@@ -24,24 +14,14 @@ export default {
     return {}
   },
   components: {
-    Cookies,
-    ModalDefault,
-    Loading,
     Header,
     Footer
   },
   computed: {
-    loading() {
-      return this.$store.state.manager.loading
-    },
-    modalInfo() {
-      return this.$store.state.manager.modalDefault
-    }
+    
   },
   mounted() {
-    // setTimeout(() => {
-    //   this.$store.commit('manager/SET_LOADING', false)
-    // }, 5000)
+    
   },
   watch: {}
 }
